f011d4cbe41d0c0f97bfcad26eb32179927af032
Jude Hungerford <edward.h@gmx.net>
2012-03-23 20:11:05 +1100

    replace tcpclient with zombie-killing one

--- clients/tcpclient.py
+++ clients/tcpclient.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 
 import time
 import sys
@@ -7,12 +7,9 @@ import re
 import string
 import random
 import subprocess
+import shlex
 from socket import socket, AF_INET, SOCK_STREAM
 
-try:
-    from io import BytesIO
-except ImportError:
-    from StringIO import StringIO as BytesIO
 
 USAGE="""
 
@@ -26,19 +23,20 @@ USAGE="""
 """
 
 
+
 def readline(sock):
-    s = BytesIO()
-    while sock:
-        c = sock.recv(1)
-        if not c:
-            break
-        elif c==b'\r':
-            continue
-        elif c==b'\n':
-            break
-        else:
-            s.write(c)
-    return s.getvalue()
+  s=""
+  while(sock):
+     c = sock.recv(1)
+     if ( not c ):
+        break
+     elif ( c=='\r' ):
+        continue
+     elif ( c=='\n' ):
+        break
+     else:
+        s += c
+  return s
 
 
 time_out = 1.0
@@ -52,114 +50,107 @@ def tcp(host, port, bot_command, user, password, options):
     sock.settimeout(240)
     sock.connect((host, port))
     if sock:
-        sys.stderr.write('\n\nconnected to %s:%d as %s\n' % (host,port,user))
+        sys.stderr.write("\n\nconnected to %s:%d as %s\n" % (host,port,user))
     else:
-        sys.stderr.write('\n\nfailed to connect to %s:%d as %s\n' % (host,port,user))
+        sys.stderr.write("\n\nfailed to connect to %s:%d as %s\n" % (host,port,user))
         return
 
     # send greetz
-    sock.sendall(('USER %s %s\n' % (user, password)).encode('utf-8'))
+    sock.sendall("USER %s %s\n" % (user, password) )
 
     # start bot
-    bot = subprocess.Popen(
-        bot_command,
-        stdin=subprocess.PIPE,
-        stdout=subprocess.PIPE,
-        shell=True,
-    )
+    try:
+        import os
+        args = shlex.split(bot_command)
+        bot = subprocess.Popen(args,
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            cwd=".")
+    except:
+        print( 'your bot ('+str(bot_command)+') failed to start!' )
+        return;
 
-    bot_input = BytesIO()
-    bot_output = BytesIO()
-
-    while sock:
-        end_reached = False
-        finish = False
-        # get input, send it to bot
+    try:
         while sock:
-            try:
-                line = readline(sock)
-            except:
-                finish = True
-                break
+            end_reached = False
+            # get input, send it to bot
+            bot_input = ""
+            while sock:
+                try:
+                    line = readline(sock)
+                except: return
 
-            if not line:
-                bot_input.write(b'end\ngo\n')
-                if end_reached:
-                    finish = True
+                if not line:
+                    if end_reached:
+                        sock.close()
+                        sock = None
+                        bot.kill()
+                        return
+                    continue # bad connection, keep on trying
+
+                print( line )
+                if line.startswith("INFO:"): # not meant for the bot
+                    if (line.find("already running")>0) or (line.find("already queued")>0):
+                        ## penalty for getting eliminated, but still trying to be first in the upcoming game.
+                        time_out += 10.0 + 10.0*random.random()
+                    continue
+
+                bot_input += line + "\n"
+                if line.startswith("end"):
+                    end_reached = True
+                if line.startswith("ready"):
+                    time_out = 1.0
                     break
-                continue # bad connection, keep on trying
+                if line.startswith("go"):
+                    if end_reached:
+                        sock.close()
+                        sock = None
+                        bot.kill()
+                        return
+                    break
 
-            print(line.decode('ascii', 'replace'))
-            if line.startswith(b'INFO:'): # not meant for the bot
-                if (line.find(b'already running')>0) or (line.find(b'already queued')>0):
-                    ## penalty for getting eliminated, but still trying to be first in the upcoming game.
-                    time_out += 10.0 + 10.0*random.random()
-                continue
-
-            bot_input.write(line)
-            bot_input.write(b'\n')
-
-            if line.startswith(b'end'):
-                end_reached = True
-            elif line.startswith(b'ready'):
-                time_out = 1.0
-                break
-            elif line.startswith(b'go'):
-                if end_reached:
-                    finish = True
+            if not sock:
                 break
 
-        bot.stdin.write(bot_input.getvalue())
-        bot.stdin.flush()
+            bot.stdin.write(bot_input)
+            bot.stdin.flush()
 
-        bot_input.seek(0)
-        bot_input.truncate()
+            # get bot output, send to serv
+            client_mess=""
+            while 1:
+                answer = bot.stdout.readline()
+                if not answer:    break
+                client_mess += answer
+                if answer.startswith("go"):    break
 
-        if finish or not sock:
-            break
+            # if there's no orders, send at least an empty line
+            if (client_mess==""):
+                client_mess="\r\n"
+            print( client_mess )
 
-        # get bot output, send to serv
-        while 1:
-            answer = bot.stdout.readline()
-            if not answer.strip():
-                break
-                
-            # weed out stuff meant for the extended visualizer
-            if answer.startswith(b'v'): continue
-            if answer.startswith(b'i'): continue
-                
-            bot_output.write(answer)
-            if answer.startswith(b'go'):
-                break
+            sock.sendall( client_mess )
+    finally:
+        try:
+            bot.kill()
+        except:
+            pass
 
-        output = bot_output.getvalue()
-        bot_output.seek(0)
-        bot_output.truncate()
 
-        # if there's no orders, send at least an empty line
-        if not output:
-            output = b'\r\n'
-        print(output.decode('ascii', 'replace'))
-        sock.sendall(output)
 
-    try:
-        sock.close()
-        sock = None
-    except:
-        pass
-
-    try:
-        bot.kill()
-        time.sleep(0.5)
-        bot.wait()
-    except:
-        pass
-
+def check_string( pname, use ):
+    """ check for invalid chars since json won't like them. """
+    for l in pname:
+        if l in string.letters: continue
+        if l in string.digits : continue
+        if l =='_' : continue
+        print( "your "+use+" (" + pname + ") contains invalid characters, please choose another one!" )
+        return False
+    return True
 
 
 def main():
     if len(sys.argv) < 6:
-        print(USAGE)
+        print USAGE
         return
 
     host = sys.argv[1]
@@ -168,14 +159,22 @@ def main():
     pname = sys.argv[4]
     password = sys.argv[5]
 
+    if not check_string(pname, "botname"):
+        return
+    if not check_string(password, "password"):
+        return
+
     try:
         rounds = int(sys.argv[6])
     except:
-        rounds = 1
+        rounds = -1
 
-    for i in range(rounds):
-        tcp(host, port, botpath, pname, password, {})
-
+    if rounds == -1:
+        while True:
+            tcp(host, port, botpath, pname, password, {})
+    else:
+        for i in range(rounds):
+            tcp(host, port, botpath, pname, password, {})
 
 if __name__ == "__main__":
     main()
