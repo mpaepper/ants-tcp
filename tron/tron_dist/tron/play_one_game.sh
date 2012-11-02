025a4fabf5f754a25aa897b8289cb9a189f0002f
Jude Hungerford <edward.h@gmx.net>
2012-11-03 04:10:05 +1100

    set sensible play_one_game map

--- tron/tron_dist/tron/play_one_game.sh
+++ tron/tron_dist/tron/play_one_game.sh
@@ -2,6 +2,5 @@
 
 ./playgame.py --verbose --fill --log_input --log_output --log_error \
 --log_dir game_logs --turns 500 --turntime 1000 \
---map_file maps/tron_03.map \
-./ocaml_starter \
-./ocaml_starter
+--map_file maps/tron_02.map \
+"python2 dist/starter_bots/python/MyBot.py"
