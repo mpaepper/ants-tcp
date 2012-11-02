113454cf480644efe37dbd57cbccaf519702e587
Jude Hungerford <edward.h@gmx.net>
2012-11-02 18:41:40 +1100

    basic working state

--- tron/tron/tron/j4_double.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-./playgame.py --verbose --fill --log_input --log_output --log_error \
---nolaunch --log_dir game_logs --turns 500 --turntime 1000 \
---map_file maps/test/test_p04_02.map \
-/home/smiley/ast/source/ocaml_asteroids/MyBot.native \
-/home/smiley/ast/source/ocaml_asteroids/MyBot.native \
-"python dist/sample_bots/python/LeftyBot.py" \
-"python dist/sample_bots/python/RandomBot.py" \
--- tron/tron/tron/j8.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-./playgame.py --verbose --fill --log_input --log_output --log_error \
---nolaunch --log_dir game_logs --turns 500 --turntime 1000 \
---map_file maps/test/test_p02_01.map \
-/home/smiley/ast/source/ocaml_asteroids/MyBot.native \
-/home/smiley/ast/source/ocaml_asteroids/MyBot.native \
